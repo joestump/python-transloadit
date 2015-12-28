183899c7c22ece51e9acca7dfa7d405b7e4f0cb1
Nicholas Serra <nicks@ydekproductions.com>
2015-12-28 00:41:57 -0500

    Fix for invalid json characters in response.

--- transloadit/client.py
+++ transloadit/client.py
@@ -1,25 +1,11 @@
 import hashlib
 import hmac
-import urlparse
-import httplib
-import mimetypes
+import json
 from datetime import datetime, timedelta
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+import requests
 
-
-try:
-    from django.utils import simplejson as json
-except ImportError:
-    import simplejson as json
-
-
-BASE_API = 'http://api2.transloadit.com/assemblies'
-FILE_BOUNDARY = '----------Python_Transloadit_Client_Boundary_$'
-CRLF = '\r\n'
+ASSEMBLY_API_URL = 'http://api2.transloadit.com/assemblies'
 
 
 class Client(object):
@@ -29,52 +15,11 @@ class Client(object):
         if api:
             self.api = api
         else:
-            self.api = BASE_API
+            self.api = ASSEMBLY_API_URL
 
     def _sign_request(self, params):
         return hmac.new(self.secret, json.dumps(params),
             hashlib.sha1).hexdigest()
-
-    def _send_request(self, files, **fields):
-        parts = urlparse.urlparse(self.api)
-        content_type, body = self._encode_request(fields, files)
-        req = httplib.HTTP(parts[1])
-        req.putrequest('POST', parts[2])
-        req.putheader('Content-Type', content_type)
-        req.putheader('Content-Length', str(len(body)))
-        req.endheaders()
-        req.send(body)
-        errcode, errmsg, headers = req.getreply()
-        return json.loads(req.file.read())
-
-    def _encode_request(self, fields, files):
-        body = StringIO()
-
-        for key, value in fields.iteritems():
-            body.write('--%s%s' % (FILE_BOUNDARY, CRLF))
-            body.write('Content-Disposition: form-data; name="%s"%s' % (key, CRLF))
-            body.write(CRLF)
-            body.write(value)
-            body.write(CRLF)
-
-        if files:
-            for key, filename, value in files:
-                content_type = self._get_content_type(filename)
-                body.write('--%s%s' % (FILE_BOUNDARY, CRLF))
-                body.write('Content-Disposition: form-data;' + \
-                    ' name="%s"; filename="%s"%s' % (key, filename, CRLF))
-                body.write('Content-Type: %s%s' % (content_type, CRLF))
-                body.write(CRLF)
-                body.write(value)
-                body.write(CRLF)
-
-        body.write('--%s--%s' % (FILE_BOUNDARY, CRLF))
-        body.write(CRLF)
-        content_type = 'multipart/form-data; boundary=%s' % FILE_BOUNDARY
-        return content_type, body.getvalue()
-
-    def _get_content_type(self, filename):
-        return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
 
     def request(self, files=None, **params):
         if 'auth' not in params:
@@ -84,9 +29,7 @@ class Client(object):
                     timedelta(days=1)).strftime('%Y/%m/%d %H:%M:%S')
             }
 
-        fields = {
-            'params': json.dumps(params),
-            'signature': self._sign_request(params)
-        }
+        response = requests.post(ASSEMBLY_API_URL, data={'params': json.dumps(params),
+            'signature': self._sign_request(params)}, files=files)
 
-        return self._send_request(files, **fields)
+        return response.json()
